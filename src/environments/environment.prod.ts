2aed4786176269abbc1de72ff4c9b27149cd202b
Luis Felipe Pinto <felipe_pinto@satrack.com>
2026-06-29 16:56:54 -0500

    Revert "esto es una prueba para subir algo"
    This reverts commit 6b939eb64781e43dd6c7da1748eb8273c2e3d7c4.

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  servicioApiUrl: '/index.php',
+  servicioApiUrl: 'https://ambulancia.urlcs.co/index.php',
   servicioUsuario: 'historiaClinica',
   servicioPassword: '4536905-8a3deb23',
   loginUser: 'admin',
